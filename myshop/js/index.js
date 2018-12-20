6eecee058ec9bb76e273f55c05f90a35ede45022
Hongliang0513 <1195528273@qq.com>
2018-12-20 23:52:36 +0800

    创建demo

---
+++ myshop/js/index.js
@@ -0,0 +1,11 @@
+let productData = null;
+let xhr = new XMLHttpRequest();
+xhr.open('GET','./json/product.json',false);
+xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4 && xhr.status === 200){
+        productData = xhr.responseText;
+    }
+};
+xhr.send(null);
+productData = JSON.parse(productData);
+console.log(productData);
